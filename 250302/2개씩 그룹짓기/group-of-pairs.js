3af60caa2a1a9c80a093f0ffd00bae372aa38ea2
Kim To Kyun <58878190+ToKyun02@users.noreply.github.com>
2025-03-02 19:30:51 +0900

    Title: 2개씩 그룹짓기, Time: 181ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250302/2개씩 그룹짓기/group-of-pairs.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const nums = input[1].trim().split(' ').map(Number);
+
+// Please Write your code here.
+
+function getGroupMaxOfMin(nums){
+    nums.sort((a,b)=>a-b);
+    let groupMax = 0;
+    for(let i=0; i<n; i++){
+        const groupSum = nums[i] + nums[2*n-1-i];
+        if(groupSum>groupMax){
+            groupMax=groupSum
+        }
+    }
+}
+
+console.log(getGroupMaxOfMin(nums));
