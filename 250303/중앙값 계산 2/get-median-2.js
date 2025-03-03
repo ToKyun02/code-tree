3d037c5328b2ecd607ef1c39c4d9d0b8c5452665
Kim To Kyun <58878190+ToKyun02@users.noreply.github.com>
2025-03-03 13:38:44 +0900

    Title: 중앙값 계산 2, Time: 179ms, Memory: 15MB, Status: Passed - CodeTree

--- 250303/중앙값 계산 2/get-median-2.js
+++ 250303/중앙값 계산 2/get-median-2.js
@@ -7,9 +7,9 @@ const arr = input[1].split(' ').map(Number);
 // Please Write your code here.
 const result = [];
 
-arr.forEach((num,index)=>{
-    if(num%2!==0) {
-        const mid = arr.slice(0,index+1)[Math.floor(index/2)];
+arr.forEach((_,index)=>{
+    if(index%2===0) {
+        const mid = arr.slice(0,index+1).sort((a,b)=>a-b)[Math.floor(index/2)];
         result.push(mid);
     }  
 })
